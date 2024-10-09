e2860738421758dc7f7734099ba458f019500684
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2024-10-09 20:12:45 -0300

    feat(CI): creating CI.yml to automate tests

--- src/main/java/com/example/comerce/core/entities/User.java
+++ src/main/java/com/example/comerce/core/entities/User.java
@@ -2,14 +2,12 @@ package com.example.comerce.core.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.UUID;
 
 @Entity
-
 @Getter
 @Setter
 public class User {
